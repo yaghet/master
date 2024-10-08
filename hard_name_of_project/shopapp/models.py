e3ce0d6a424e467a8d5a0a1574ce506249dbd3d3
Виктор Бовыкин <aimsscs@gmail.com>
2024-10-08 13:15:35 +0300

    add field 'created_by'

--- hard_name_of_project/shopapp/models.py
+++ hard_name_of_project/shopapp/models.py
@@ -12,6 +12,7 @@ class Product(models.Model):
     price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
     discount = models.SmallIntegerField(default=0)
     create_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='products', null=True)
     archive = models.BooleanField(default=False)
 
 
