01cf8ab88296f8a823f32d3a4841df9330f17042
Виктор Бовыкин <aimsscs@gmail.com>
2025-03-10 21:05:57 +0300

    change gettext to lazy and add some validators

--- DRF/mysite/shopapp/models.py
+++ DRF/mysite/shopapp/models.py
@@ -1,47 +1,76 @@
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
-def product_preview_directory_path(instance: "Product", filename: str) -> str:
-    return "products/product_{pk}/preview/{filename}".format(
+def product_preview_directory_path(instance: 'Product', filename: str) -> str:
+
+    """
+
+    Функция для генерации 'path' в корневой папке директории для хранения 'preview' продукта
+
+    """
+
+    return 'products/product_{pk}/preview/{filename}'.format(
         pk=instance.pk,
         filename=filename,
     )
 
 
-class Product(models.Model):
-    class Meta:
-        ordering = ["name", "price"]
+def product_images_directory_path(instance: 'ProductImage', filename: str) -> str:
 
-    name = models.CharField(max_length=100)
-    description = models.TextField(null=False, blank=True)
-    price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
-    discount = models.SmallIntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
-    archived = models.BooleanField(default=False)
-    preview = models.ImageField(null=True, blank=True, upload_to=product_preview_directory_path)
+    """
 
-    def __str__(self):
-        return f"Product(pk={self.pk}, name={self.name!r})"
+    Функция для генерации 'path' в корневой папке директории для хранения 'images' продукта
 
+    """
 
-def product_images_directory_path(instance: "ProductImage", filename: str) -> str:
-    return "products/product_{pk}/images/{filename}".format(
+    return 'products/product_{pk}/images/{filename}'.format(
         pk=instance.product.pk,
         filename=filename,
     )
 
 
+class Product(models.Model):
+
+    objects = None
+
+    class Meta:
+        verbose_name = _('Product')
+        verbose_name_plural = _('Products')
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=False, blank=True)
+    price = models.DecimalField(default=0, max_digits=8, decimal_places=2, validators=[MinValueValidator(0)])
+    discount = models.SmallIntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
+    create_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='products', null=True)
+    archive = models.BooleanField(default=False)
+    preview = models.ImageField(null=True, blank=True, upload_to=product_preview_directory_path)
+
+
+    def __str__(self) -> str:
+        return f'Product pk={self.pk} name={self.name!r}'
+
+
 class ProductImage(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
+    objects = None
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
     image = models.ImageField(upload_to=product_images_directory_path)
-    description = models.CharField(max_length=200, null=False, blank=True)
+    description = models.TextField(max_length=200, null=False, blank=True)
 
 
 class Order(models.Model):
+    class Meta:
+        verbose_name = _('Order')
+        verbose_name_plural = _('Orders')
+
+    objects = None
+
     delivery_address = models.TextField(null=True, blank=True)
     promocode = models.CharField(max_length=20, null=False, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
-    products = models.ManyToManyField(Product, related_name="orders")
+    create_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='orders')
+    products = models.ManyToManyField(Product, related_name='orders')
     receipt = models.FileField(null=True, upload_to='orders/receipts/')
