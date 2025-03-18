987e41fd36a44d349ce2b7032fcf3af698505f66
Виктор Бовыкин <aimsscs@gmail.com>
2025-03-18 18:08:03 +0300

    reformat repository

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hvxn%qq=gyw^4*o2lo1#bw0=wh#ux9s8h!=@c608arf_gz3+^7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
